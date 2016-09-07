d6e0ade76d79e0746ff17db6a5c280c208fba6c4
Kevin Martensson <kevinmartensson@gmail.com>
2016-09-07 23:41:55 +0200

    Improve regex
    Fixes #1.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = function (opts) {
 	opts = opts || {};
-	var regex = '(?:[13][1-9A-Za-z][^O0Il]{24,32})';
+	var regex = '(?:[13][a-km-zA-HJ-NP-Z1-9]{25,34})';
 
 	return opts.exact ? new RegExp('(?:^' + regex + '$)') :
 						new RegExp(regex, 'g');
